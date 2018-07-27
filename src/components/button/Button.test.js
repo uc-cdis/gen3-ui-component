92a247ce28e995e50a55c4d2a42de51b24c83096
Qingya Shu <qingya.shu@gmail.com>
2018-07-27 09:45:37 -0500

    fix(init): update nodejs version and fix typo

--- src/components/button/Button.test.js
+++ src/components/button/Button.test.js
@@ -3,7 +3,7 @@ import { mount } from 'enzyme';
 import Button from './Button';
 
 describe('<Button />', () => {
-  const btn = mount(<Button label="tes-button"></Button>).find(Button);
+  const btn = mount(<Button label="test-button"></Button>).find(Button);
 
   it('renders', () => {
     expect(btn.length).toBe(1);
