066cfd2d9640dd479c85f589aa4490522b5fe172
Qingya Shu <qingya.shu@gmail.com>
2018-07-30 09:56:32 -0500

    fix(init): enable eslint

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -5,17 +5,15 @@ import './Button.css';
 class Button extends Component {
   render() {
     return (
-      <button className="btn-normal">{this.props.label}</button>
+      <button type="button" className="btn-normal">
+        {this.props.label}
+      </button>
     );
   }
 }
 
 Button.propTypes = {
   label: PropTypes.string.isRequired,
-};
-
-Button.defaultProps = {
-  label: 'click me!',
 };
 
 export default Button;
