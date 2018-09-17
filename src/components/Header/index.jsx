624e0adca4d5df6272c5a6ea0157a21da85034e3
abgeorge7 <abgeorge@umich.edu>
2018-09-17 09:48:39 -0500

    added basic header for demo framework

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className='header'>
+        <img className='header__logo' src={this.props.logoSrc} />
+        <h1 className='header__title'>{this.props.title}</h1>
+        <div className='header__logo' />
+      </div>
+    );
+  }
+};
+
+Header.propTypes = {
+  logoSrc: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
