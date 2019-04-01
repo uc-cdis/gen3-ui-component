b3706cddf698c3c15f7e5f870a918cd5753e54d9
Benjamin Rohrer <benJrohrer@gmail.com>
2019-04-01 15:53:02 -0500

    fix(Toaster): adding proptypes entry for children

--- src/components/Toaster/Toaster.jsx
+++ src/components/Toaster/Toaster.jsx
@@ -3,21 +3,23 @@ import PropTypes from 'prop-types';
 import './Toaster.css';
 
 class Toaster extends React.Component {
-
   render() {
-    return this.props.isEnabled() ?
-      (
+    return this.props.isEnabled()
+      ? (
         <div className='toaster__div'>
           {this.props.children}
         </div>
       )
-      : null
+      : null;
   }
-
 }
 
 Toaster.propTypes = {
   isEnabled: PropTypes.func.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
 };
 
 export default Toaster;
