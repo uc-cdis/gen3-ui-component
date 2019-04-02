bb73c3acdd9c85de012cfd37ab45c0932a884fca
Benjamin Rohrer <benJrohrer@gmail.com>
2019-04-02 15:33:10 -0500

    fix(Toaster): fix classname prop

--- src/components/Toaster/Toaster.test.jsx
+++ src/components/Toaster/Toaster.test.jsx
@@ -3,21 +3,19 @@ import { mount } from 'enzyme';
 import Toaster from '.';
 
 describe('<Toaster />', () => {
-
-    const t = mount(
-      <Toaster isEnabled={false}>
-        <div>
+  const t = mount(
+    <Toaster isEnabled={false}>
+      <div>
           Test
-        </div>
-      </Toaster>
-    );
+      </div>
+    </Toaster>,
+  );
 
-    it('updates with state change', () => {
-      expect(t.find('.toaster__div').length).toBe(0);
-      t.setProps({isEnabled: true});
-      expect(t.find('.toaster__div').length).toBe(1);
-      t.setProps({isEnabled: false});
-      expect(t.find('.toaster__div').length).toBe(0);
-    });
-
+  it('updates with state change', () => {
+    expect(t.find('.toaster__div').length).toBe(0);
+    t.setProps({ isEnabled: true });
+    expect(t.find('.toaster__div').length).toBe(1);
+    t.setProps({ isEnabled: false });
+    expect(t.find('.toaster__div').length).toBe(0);
+  });
 });
--- src/components/Toaster/index.jsx
+++ src/components/Toaster/index.jsx
@@ -6,7 +6,7 @@ class Toaster extends React.Component {
   render() {
     return this.props.isEnabled
       ? (
-        <div className={`${this.props.className} toaster__div`>
+        <div className={`${this.props.className} toaster__div`}>
           {this.props.children}
         </div>
       )
@@ -25,7 +25,7 @@ Toaster.propTypes = {
 };
 
 Toaster.defaultProps = {
-  className: ''
-}
+  className: '',
+};
 
 export default Toaster;
