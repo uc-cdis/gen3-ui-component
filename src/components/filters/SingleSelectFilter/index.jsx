75fa42154cd87f187dde451fb9da3a7e21f60492
Mingfei Shao <mshao1@uchicago.edu>
2019-04-26 17:09:37 -0500

    feat/disp lock and under for filter

--- src/components/filters/SingleSelectFilter/index.jsx
+++ src/components/filters/SingleSelectFilter/index.jsx
@@ -21,6 +21,25 @@ class SingleSelectFilter extends React.Component {
     }
     // Takes in parent component's selected or self state's selected
     const selected = typeof this.props.selected === 'undefined' ? this.state.selected : this.props.selected;
+    let rightIcon;
+    let inputDisabled = false;
+
+    if (this.props.count === this.props.hideValue) {
+      rightIcon = (
+        <React.Fragment>
+          <span className='g3-badge single-select-filter__count'>{this.props.hideLimit}</span>
+          <i className='g3-icon g3-icon--md g3-icon--under g3-color__black' />
+        </React.Fragment>
+      );
+      inputDisabled = true;
+    } else {
+      rightIcon = <span className='g3-badge single-select-filter__count'>{this.props.count}</span>;
+    }
+
+    if (this.props.displayLock) {
+      rightIcon = <i className='g3-icon g3-icon--md g3-icon--lock g3-color__black' />;
+    }
+
     return (
       <div className='single-select-filter'>
         <input
@@ -28,9 +47,10 @@ class SingleSelectFilter extends React.Component {
           type='checkbox'
           onChange={() => this.handleCheck()}
           checked={selected}
+          disabled={inputDisabled}
         />
         <p className='single-select-filter__label'>{this.props.label}</p>
-        <span className='g3-badge single-select-filter__count'>{this.props.count}</span>
+        { rightIcon }
       </div>
     );
   }
@@ -42,12 +62,18 @@ SingleSelectFilter.propTypes = {
   selected: PropTypes.bool,
   count: PropTypes.number,
   hideZero: PropTypes.bool,
+  hideValue: PropTypes.number,
+  hideLimit: PropTypes.number,
+  displayLock: PropTypes.number,
 };
 
 SingleSelectFilter.defaultProps = {
   selected: undefined,
   count: 0,
   hideZero: true,
+  hideValue: -1,
+  hideLimit: 1000,
+  displayLock: 0,
 };
 
 export default SingleSelectFilter;
