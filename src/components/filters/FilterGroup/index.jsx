303be3c41af12d1a216bfbdd5f97908156efba7f
Mingfei Shao <mshao1@uchicago.edu>
2019-05-07 22:16:52 -0500

    fix/show all collapse all as one button

--- src/components/filters/FilterGroup/index.jsx
+++ src/components/filters/FilterGroup/index.jsx
@@ -17,13 +17,16 @@ const removeEmptyFilter = (filterResults) => {
 class FilterGroup extends React.Component {
   constructor(props) {
     super(props);
+    const initialExpandedStatusControl = true;
     const initialExpandedStatus = props.filterConfig.tabs
-      .map(t => t.fields.map(() => (true)));
+      .map(t => t.fields.map(() => (initialExpandedStatusControl)));
     const initialFilterStatus = props.filterConfig.tabs
       .map(t => t.fields.map(() => ({})));
     this.state = {
       selectedTabIndex: 0,
       expandedStatus: initialExpandedStatus,
+      expandedStatusText: 'Collapse all',
+      expandedStatusControl: initialExpandedStatusControl,
 
       /**
        * Current selected status for filters,
@@ -143,11 +146,14 @@ class FilterGroup extends React.Component {
     this.props.onFilterChange(this.state.filterResults);
   }
 
-  toggleFilters(openAll) {
-    const newExpandedStatus = this.props.filterConfig.tabs
-      .map(t => t.fields.map(() => (openAll)));
-    this.setState({ expandedStatus: newExpandedStatus });
-    this.currentFilterListRef.current.toggleFilters(openAll);
+  toggleFilters() {
+    this.setState(prevState => ({
+      expandedStatus: this.props.filterConfig.tabs
+        .map(t => t.fields.map(() => (!prevState.expandedStatusControl))),
+      expandedStatusText: (!prevState.expandedStatusControl) ? 'Collapse all' : 'Open all',
+      expandedStatusControl: !prevState.expandedStatusControl,
+    }));
+    this.currentFilterListRef.current.toggleFilters();
   }
 
   render() {
@@ -174,21 +180,12 @@ class FilterGroup extends React.Component {
         <div className='filter-group__collapse'>
           <span
             className='g3-link filter-group__collapse-link'
-            onClick={() => this.toggleFilters(true)}
-            onKeyPress={() => this.toggleFilters(true)}
+            onClick={() => this.toggleFilters()}
+            onKeyPress={() => this.toggleFilters()}
             role='button'
             tabIndex={0}
           >
-            Show all
-          </span>
-          <span
-            className='g3-link filter-group__collapse-link'
-            onClick={() => this.toggleFilters(false)}
-            onKeyPress={() => this.toggleFilters(false)}
-            role='button'
-            tabIndex={0}
-          >
-            Collapse all
+            {this.state.expandedStatusText}
           </span>
         </div>
         <div className='filter-group__filter-area'>
