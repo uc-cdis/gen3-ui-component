6e26907a5cfbad3aa8042042a60a6ae5f85e19ad
Zakir Gowani <zakirgowani@gmail.com>
2019-11-14 15:01:52 -0600

    fix chrome icon issuer

--- src/components/charts/helper.js
+++ src/components/charts/helper.js
@@ -116,6 +116,7 @@ const getCharts = (data, dataExplorerConfig, sqon) => {
           break;
         case 'pie':
         case 'bar':
+          console.log('inside getCharts with fields ', fields, ' fieldConfig ', fieldConfig, ' field:  ', field);
           summaries.push(
             transformArrangerDataToSummary(
               fields[field],
