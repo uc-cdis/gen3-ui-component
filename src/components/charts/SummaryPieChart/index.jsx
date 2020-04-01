7bf8517529c9f800cf8ea1c009565119a7617a4f
Zakir Gowani <zakirgowani@gmail.com>
2020-04-01 16:24:59 -0500

    feat: show values by pie chart percentages (#66)
    * feat: show values by pie chart percentages

--- src/components/charts/SummaryPieChart/index.jsx
+++ src/components/charts/SummaryPieChart/index.jsx
@@ -56,10 +56,13 @@ class SummaryPieChart extends React.Component {
                         {entry.name}
                       </div>
                       <div className='summary-pie-chart__legend-item-value form-special-number'>
-                        {
+                      <span className='summary-pie-chart__legend-item-value-number'>
+                        { helper.numberWithCommas(entry.value) }
+                      </span> 
+                      <span className='summary-pie-chart__legend-item-value-percentage'> ({
                           helper
                             .percentageFormatter(this.props.showPercentage)(entry[dataKey])
-                        }
+                      })</span>
                       </div>
                     </div>
                   );
