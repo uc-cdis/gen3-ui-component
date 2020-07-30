703fa79bd83e5135fa2c4265c00674111e2d68db
Michael Ingram <mpingram@uchicago.edu>
2020-07-30 10:31:51 -0500

    Use select library to implement search filter

--- stories/filters.js
+++ stories/filters.js
@@ -38,6 +38,17 @@ const raceOptions = [
   { text: 'Other', filterType: 'singleSelect', count: 123 },
 ];
 
+const guidOptions = [];
+const NUM_GUID_OPTIONS = 500000;
+for (let i = 0; i < NUM_GUID_OPTIONS; i += 1) {
+  guidOptions.push({
+    text: `guid-${i}`,
+    filterType: 'singleSelect',
+    count: i,
+    accessible: true,
+  });
+}
+
 const ethnicityOptions = [
   {
     text: 'Hispanic or Latino', filterType: 'singleSelect', count: 123, accessible: true,
@@ -178,6 +189,16 @@ storiesOf('Filters', module)
       onSelect={action('checked')}
       onAfterDrag={action('range change')}
       tierAccessLimit={1000}
+    />
+  ))
+  .add('SearchFilter', () => (
+    <FilterSection
+      title={'File GUIDs'}
+      options={guidOptions}
+      onSelect={action('checked')}
+      onAfterDrag={action('range change')}
+      tierAccessLimit={1000}
+      isSearchFilter={true}
     />
   ))
   .add('FilterList', () => (
