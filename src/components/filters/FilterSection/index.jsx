fc40cdea84ac3e3202185127ced353d769f84eec
Zakir Gowani <zakirgowani@gmail.com>
2020-09-29 14:52:07 -0500

    fix width

--- src/components/filters/FilterSection/index.jsx
+++ src/components/filters/FilterSection/index.jsx
@@ -87,7 +87,7 @@ class FilterSection extends React.Component {
     return (
       <React.Fragment>
         <div className={`g3-filter-section__and-or-toggle ${isHidden && 'g3-filter-section__hidden'}`}>
-          <span style={{"margin-right":"5px"}}>Combine with </span>
+          <span style={{marginRight:"5px"}}>Combine with </span>
           <Radio.Group defaultValue={this.state.combineMode} buttonStyle='solid'>
             <Radio.Button value='AND' onChange={() => this.handleSetCombineModeOption('AND')}>
             AND
