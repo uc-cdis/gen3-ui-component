6bb8f4c3d42a37ac8cdeadb2911fc9b57c860459
Zakir Gowani <zakirgowani@gmail.com>
2021-06-21 15:15:53 -0500

    fix: tab navigation issues

--- src/components/filters/FilterSection/index.jsx
+++ src/components/filters/FilterSection/index.jsx
@@ -92,31 +92,38 @@ class FilterSection extends React.Component {
         <div className={`g3-filter-section__and-or-toggle ${isHidden && 'g3-filter-section__hidden'}`}>
           <span style={{ marginRight: '5px' }}>Combine with </span>
           <Radio.Group defaultValue={this.state.combineMode} buttonStyle='solid'>
-            <Radio.Button value='AND' 
+            <Radio.Button
+              value='AND'
               onKeyPress={(event) => {
                 console.log(event);
-                if (event.key === 'Enter') {
+                console.log(event.key);
+                console.log(event.key());
+                if (event.key() === 'Enter') {
                   this.handleSetCombineModeOption('AND');
                 }
               }}
               onClick={() => this.handleSetCombineModeOption('AND')}
               onChange={() => this.handleSetCombineModeOption('AND')}
               tabIndex='0'
-              className='g3-ring-on-focus'>
+              className='g3-ring-on-focus'
+            >
             AND
             </Radio.Button>
-            <Radio.Button 
+            <Radio.Button
               value='OR'
               onKeyPress={(event) => {
                 console.log(event);
-                if (event.key === 'Enter') {
+                console.log(event.key);
+                console.log(event.key());
+                if (event.key() === 'Enter') {
                   this.handleSetCombineModeOption('OR');
                 }
               }}
               onClick={() => this.handleSetCombineModeOption('OR')}
               onChange={() => this.handleSetCombineModeOption('OR')}
               tabIndex='0'
-              className='g3-ring-on-focus'>
+              className='g3-ring-on-focus'
+            >
               OR
             </Radio.Button>
           </Radio.Group>
