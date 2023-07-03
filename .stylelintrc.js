236fe1b7fb8f00b493e767177ea18ddd0160d221
Shawn O'Connor <ocshawn@ocshawn.com>
2023-07-03 10:47:19 -0500

    update all dependencies and resolve all warnings and security vulnerabilities (#151)
    Co-authored-by: ocshawn <shawnoconnor@uchicago.edu>

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,14 +1,7 @@
 module.exports = {
   "customSyntax": "postcss-less",
   "rules": {
-    "indentation": 2,
     "max-nesting-depth": 1,
-    "declaration-colon-space-after": "always",
-    "declaration-colon-space-before": "never",
-    "declaration-block-semicolon-newline-after": "always",
-    "block-closing-brace-newline-after": "always",
-    "block-opening-brace-newline-after": "always",
-    "block-opening-brace-space-before": "always",
     "color-no-invalid-hex": true,
     "unit-no-unknown": true,
     "property-no-unknown": true,
