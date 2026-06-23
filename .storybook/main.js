f090075f5f3246857ea0776cbb1acdb7995f0406
Mingfei Shao <mshao1@uchicago.edu>
2026-06-23 09:56:11 -0500

    update to storybook 10

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,41 @@
+import path from 'path';
+
+/** @type { import('storybook').StorybookConfig } */
 const config = {
-  // Required
+  stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+  ],
   framework: {
     name: '@storybook/react-webpack5',
-    options: {}
+    options: {},
   },
-  stories: ['../stories/*.stories.js'],
-  // Optional
-  addons: [],
+  webpackFinal: async (config) => {
+    config.module.rules.unshift({
+      test: /\.(js|jsx)$/,
+      include: [
+        path.resolve(import.meta.dirname, '../stories'),
+        path.resolve(import.meta.dirname, '../src')
+      ],
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: { chrome: '100' } }],
+            ['@babel/preset-react', { runtime: 'automatic' }]
+          ],
+        },
+      },
+    });
+
+    config.module.rules.push({
+      test: /\.less$/,
+      use: ['style-loader', 'css-loader', 'less-loader'],
+      include: path.resolve(import.meta.dirname, '../'),
+    });
+
+    return config;
+  },
 };
 
 export default config;
