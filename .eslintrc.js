18f5b75ed9a3d1f5b7dc4f271072f2b0ceefcd18
Mingfei Shao <mshao1@uchicago.edu>
2026-06-23 11:43:40 -0500

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,43 @@
 module.exports = {
-  "extends": ["airbnb", "plugin:storybook/recommended"],
-  "parser": "@babel/eslint-parser",
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest": true
+  env: {
+    browser: true,
+    es6: true, // or es6: true
+    node: true,
   },
-  "parserOptions": {
-    "requireConfigFile": false,
-    "babelOptions": {
-      "presets": ["@babel/preset-react"],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    babelOptions: {
+      presets: ['@babel/preset-react'],
     },
   },
-  "plugins": ["react"],
-  "rules": {
-    "linebreak-style": ["error", "unix"],
-    "quotes": ["error", "single"],
-    "jsx-quotes": ["error", "prefer-single"],
-    "semi": ["error", "always"],
-    "import/extensions": ["off", "never"],
-    "react/jsx-indent": "off",
-    "react/forbid-prop-types": "off",
-    "react/prefer-stateless-function": "off",
-    "react/jsx-curly-brace-presence": ["off", "ignore"],
-    "function-paren-newline": ["off", "ignore"],
-    "react/no-array-index-key": ["off", "ignore"],
-    "react/destructuring-assignment": ["off", "ignore"],
-    "react/jsx-filename-extension": ["off", "ignore"],
-    "react/jsx-props-no-spreading": ["off"]
-  }
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'semi': ['error', 'always'],
+    'import/extensions': ['off', 'never'],
+    'react/jsx-indent': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/prefer-stateless-function': 'off',
+    'react/jsx-curly-brace-presence': ['off', 'ignore'],
+    'function-paren-newline': ['off', 'ignore'],
+    'react/no-array-index-key': ['off', 'ignore'],
+    'react/destructuring-assignment': ['off', 'ignore'],
+    'react/jsx-filename-extension': ['off', 'ignore'],
+    'react/jsx-props-no-spreading': ['off']
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
